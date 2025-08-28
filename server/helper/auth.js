d53667e388217fcf58724bbd5aa7bcd5611ae0a8
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-08-28 15:50:21 +0300

    part 6 valmis

--- server/helper/auth.js
+++ server/helper/auth.js
@@ -17,19 +17,3 @@ const auth =  (req,res,next) => {
 }
 
 export {auth}
-
-/* import jwt from 'jsonwebtoken'
-const { verify } = jwt
-const auth = (req,res,next) => {
- const token = req.headers['authorization']
- if(!token) {
- return res.status(401).json({ message: 'No token provided' })
- }
- verify(token, process.env.JWT_SECRET, (err, decoded) => {
- if(err) {
- return res.status(401).json({ message: 'Failed to authenticate token' })
- }
- next()
- })
-}
-export { auth } */
